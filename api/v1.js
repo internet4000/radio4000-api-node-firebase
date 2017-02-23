9b8e6dfc838478f6e6d8aacde9fc8adb426b08a7
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-23 16:37:03 +0100

    filter requests and build response type

--- api/v1.js
+++ api/v1.js
@@ -4,62 +4,62 @@ var router = express.Router()
 // var cloudinary = require('cloudinary');
 
 var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 };
 
 var cloudinaryConfig = {
-    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
 };
 
 firebase.initializeApp(firebaseConfig);
 
 var notAnEndpoint = function (req, res) {
-    res.status(500).json({ error: 'Impossible to request this endpoint' });
+  res.status(500).json({ error: 'Impossible to request this endpoint' });
 };
 
 router.get('/channels', function (req, res) {
-    // TODO: remove tracks in reponse (impossible at firebase query)
-    var channels = firebase.database().ref('/channels');
-    channels.once('value').then(snapshot => {
-	res.json(snapshot.val());
-    });
+  // TODO: remove tracks in reponse (impossible at firebase query)
+  var channels = firebase.database().ref('/channels');
+  channels.once('value').then(snapshot => {
+		res.json(snapshot.val());
+  });
 });
 
 router.get('/channels/:channelSlug', function (req, res) {
-    var ref = firebase.database().ref('channels');
-    var slug = req.params.channelSlug;
-    console.log( "slug", slug );
-    ref.orderByChild('slug').equalTo(slug).once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
+  var ref = firebase.database().ref('channels');
+  var slug = req.params.channelSlug;
+  console.log( "slug", slug );
+  ref.orderByChild('slug').equalTo(slug).once('value').then(snapshot => {
+		res.send(snapshot.val());
+  }).catch(() => {
+		res.status(500).json({ error: 'Data does not exist' });
+  });
 });
 
 router.get('/tracks', notAnEndpoint);
 
 router.get('/tracks/:trackId', function (req, res) {
-    var ref = firebase.database().ref(`tracks/${req.params.trackId}`);
-    ref.once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
+  var ref = firebase.database().ref(`tracks/${req.params.trackId}`);
+  ref.once('value').then(snapshot => {
+		res.send(snapshot.val());
+  }).catch(() => {
+		res.status(500).json({ error: 'Data does not exist' });
+  });
 });
 
 router.get('/images', notAnEndpoint);
 
 router.get('/images/:imageId', function (req, res) {
-    // TODO: make cloudinary request
-    var ref = firebase.database().ref(`images/${req.params.imageId}`);
-    ref.once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
+  // TODO: make cloudinary request
+  var ref = firebase.database().ref(`images/${req.params.imageId}`);
+  ref.once('value').then(snapshot => {
+		res.send(snapshot.val());
+  }).catch(() => {
+		res.status(500).json({ error: 'Data does not exist' });
+  });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 require('dotenv').config();
 var express = require('express');
+var is = require('type-is');
 var app = express();
 var apiV1 = require('./api/v1.js');
 
 
+app.use(function(req, res, next) {
+	var hasAnyMediaType = is.is(mediaType, ['*']);
+	var hasAcceptHeader = req.get('Accept');
+
+	if (hasAnyMediaType) {
+		// 415 Unsupported Media Type, if any media type in request
+		res.status(415);
+	} else if (hasAcceptHeader) {
+		// 406 Not Acceptable
+		res.status(406);
+	}
+
+  res.set('Content-Type', 'application/vnd.api+json');
+  next();
+});
+
 app.get('/', function (req, res) {
-    // TODO: send some doczz
-    res.json({
-	"documentationUrl": "https://github.com/internet4000/radio4000-api-docs",
-	"hello": "Welcome to the Radio4000 API."
-    });
+  res.json({
+		"documentationUrl": "https://github.com/internet4000/radio4000-api-docs",
+		"hello": "Welcome to the Radio4000 API.",
+		"tes": "tessst"
+  });
 });
 
 app.use('/api/v1', apiV1);
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 });
