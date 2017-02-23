59c3637960c7bc5525582e6eb11ebedf3b74d361
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-23 02:56:19 +0100

    move api under api/version/

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 var express = require('express');
 var app = express();
-var firebase = require('firebase');
-// var cloudinary = require('cloudinary');
+var apiV1 = require('./api/v1.js');
 
-var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-};
-
-var cloudinaryConfig = {
-    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
-};
-
-firebase.initializeApp(firebaseConfig);
-
-var notAnEndpoint = function (req, res) {
-    res.status(500).json({ error: 'Impossible to request this endpoint' });
-};
 
 app.get('/', function (req, res) {
     // TODO: send some doczz
@@ -29,47 +12,8 @@ app.get('/', function (req, res) {
     });
 });
 
-app.get('/channels', function (req, res) {
-    // TODO: remove tracks in reponse (impossible at firebase query)
-    var channels = firebase.database().ref('/channels');
-    channels.once('value').then(snapshot => {
-	res.json(snapshot.val());
-    });
-});
+app.use('/api/v1', apiV1);
 
-app.get('/channels/:channelSlug', function (req, res) {
-    var ref = firebase.database().ref('channels');
-    var slug = req.params.channelSlug;
-    console.log( "slug", slug );
-    ref.orderByChild('slug').equalTo(slug).once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
-});
-
-app.get('/tracks', notAnEndpoint);
-
-app.get('/tracks/:trackId', function (req, res) {
-    var ref = firebase.database().ref(`tracks/${req.params.trackId}`);
-    ref.once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
-});
-
-app.get('/images', notAnEndpoint);
-
-app.get('/images/:imageId', function (req, res) {
-    // TODO: make cloudinary request
-    var ref = firebase.database().ref(`images/${req.params.imageId}`);
-    ref.once('value').then(snapshot => {
-	res.send(snapshot.val());
-    }).catch(() => {
-	res.status(500).json({ error: 'Data does not exist' });
-    });
-});
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 });
