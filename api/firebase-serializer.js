da8383dee75be935cd5a51a1bd9e7348c5312149
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-23 22:08:36 +0100

    refactor serializer, add track

---
+++ api/firebase-serializer.js
@@ -0,0 +1,24 @@
+var convertHasMany = function (fromObject) {
+	if (!fromObject) {
+		return;
+	}
+	return Object.keys(fromObject);
+}
+
+function serializeChannel (channel, channelId) {
+	channel.type = 'channel';
+	channel.id = channelId;
+	channel.tracks = convertHasMany(channel.tracks);
+	channel.favoriteChannels = convertHasMany(channel.favoriteChannels);
+	channel.images = convertHasMany(channel.images);
+	return channel;
+}
+
+function serializeTrack (track ,trackId) {
+	console.log(track, trackId);
+	track.id = trackId;
+	track.type = 'track';
+	return track;
+}
+
+module.exports = {serializeChannel, serializeTrack};
--- api/v1.js
+++ api/v1.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var firebase = require('firebase');
-var serializeChannel = require('../serialize-channel.js')
+var {serializeChannel, serializeTrack} = require('./firebase-serializer.js')
 var router = express.Router();
 // var cloudinary = require('cloudinary');
 
@@ -24,8 +24,14 @@ router.get('/channels', function (req, res) {
   // TODO: remove tracks in reponse (impossible at firebase query)
   var channels = firebase.database().ref('/channels');
   channels.once('value').then(snapshot => {
-		res.json(snapshot.val());
-  });
+		var val = snapshot.val();
+		var channels = Object.keys(val).map(channelId => serializeChannel(val[channelId], channelId));
+		res.json(channels);
+  }).catch(e => {
+		console.log( e );
+
+		res.status(500).json({ error: 'Data does not exist' });
+	});
 });
 
 router.get('/channels/:channelSlug', function (req, res) {
@@ -48,7 +54,9 @@ router.get('/tracks', notAnEndpoint);
 router.get('/tracks/:trackId', function (req, res) {
   var ref = firebase.database().ref(`tracks/${req.params.trackId}`);
   ref.once('value').then(snapshot => {
-		res.send(snapshot.val());
+		var track = snapshot.val();
+		var trackId = Object.keys(track);
+		res.send(serializeTrack(track, req.params.trackId));
   }).catch(() => {
 		res.status(500).json({ error: 'Data does not exist' });
   });
