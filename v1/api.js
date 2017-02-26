41c3eb6f4ac5fb2849aca02c5bcf6bd7e5e2c411
hellerve <veit@veitheller.de>
2017-02-26 19:22:19 +0100

    initial cleanup

--- v1/api.js
+++ v1/api.js
@@ -3,9 +3,16 @@ var {serializeChannel, serializeTrack, serializeImage} = require('./firebase-ser
 var {apiGetImage, apiGetTrack, apiGetChannel, apiGetChannelTracks, apiGetChannels, apiGet, apiQuery} = require('./firebase-adapter.js')
 var router = express.Router();
 
-var notAnEndpoint = function (req, res) {
-  res.status(500).json({ error: 'Impossible to request this endpoint' });
+function notAnEndpoint(req, res) {
+  res.status(404).json({ message: 'Impossible to request this endpoint' });
 };
+
+function handleError(res) {
+  return (e) => {
+		console.log(e);
+		res.status(404).json({ message: 'Data does not exist', error: e.message });
+  };
+}
 
 router.get('/', function (req, res) {
 	res.redirect('/');
@@ -13,52 +20,27 @@ router.get('/', function (req, res) {
 
 router.get('/channels', function (req, res) {
   // TODO: remove tracks in reponse (impossible at firebase query)
-	apiGetChannels().then(channels => {
-		res.json(channels);
-	}).catch(e => {
-		console.log( e );
-		res.status(500).json({ error: 'Data does not exist' });
-	});
+	apiGetChannels().then(res.json).catch(handleErrpr(res));
 });
 
 router.get('/channels/:channelSlug', function (req, res) {
-	apiGetChannel(req.params.channelSlug).then(channel => {
-		res.json(channel);
-	}).catch(e => {
-		console.log( e );
-		res.status(500).json({
-			message: 'Data does not exist',
-			error: e.message
-		});
-	});
+	apiGetChannel(req.params.channelSlug).then(res.json).catch(handleError(res));
 });
 
 router.get('/channels/:channelSlug/tracks', function (req, res) {
-	apiGetChannelTracks(req.params.channelSlug).then(tracks => {
-		res.json(tracks);
-	}).catch(e => {
-		console.log( e );
-		res.status(500).json({ error: 'Data does not exist' });
-  });
+	apiGetChannelTracks(req.params.channelSlug).then(res.json).catch(handleError(res));
 });
 
 router.get('/tracks', notAnEndpoint);
 
 router.get('/tracks/:trackId', function (req, res) {
-  apiGetTrack(req.params.trackId).then(track => res.json(track))
-		.catch(e => {
-			console.log( e );
-			res.status(500).json({ error: 'Data does not exist' });
-		});
+  apiGetTrack(req.params.trackId).then(res.json).catch(handleError(res));
 });
 
 router.get('/images', notAnEndpoint);
 
 router.get('/images/:imageId', function (req, res) {
-	apiGetImage(req.params.imageId).then(image => res.json(image)).catch(e => {
-		console.log( e );
-		res.status(500).json({ error: 'Data does not exist' });
-  });
+	apiGetImage(req.params.imageId).then(res.json).catch(handleError(res));
 });
 
 module.exports = router;
--- v1/firebase-serializer.js
+++ v1/firebase-serializer.js
@@ -1,14 +1,12 @@
 var {buildCloudinaryUrl} = require('./cloudinary-adapter.js');
 
-var convertHasMany = function (fromObject) {
-	if (!fromObject) {
-		return;
-	}
+function convertHasMany(fromObject) {
+	if (!fromObject) return;
 	return Object.keys(fromObject);
 }
 
 function serializeChannel (channel, channelId) {
-	if (!channel) {return}
+	if (!channel) return;
 	channel.type = 'channel';
 	channel.id = channelId;
 	channel.tracks = convertHasMany(channel.tracks);
@@ -20,14 +18,14 @@ function serializeChannel (channel, channelId) {
 }
 
 function serializeTrack (track ,trackId) {
-	if (!track) {return}
+	if (!track) return;
 	track.id = trackId;
 	track.type = 'track';
 	return track;
 }
 
 function serializeImage (image, imageId) {
-	if (!image) {return}
+	if (!image) return;
 	image.src = buildCloudinaryUrl(image.src);
 	image.id = imageId;
 	return image;
