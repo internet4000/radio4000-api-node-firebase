ae081007ae00be4b6ac916b7cd1a2892171766ba
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-31 20:52:12 +0200

    make it fault tollerent to previous player api

--- utils/getIframe.js
+++ utils/getIframe.js
@@ -1,12 +1,22 @@
 const fs = require('fs')
 
 exports.getIframe = (slug) => {
-	const scriptUrl = 'https://rawgit.com/internet4000/radio4000-player-vue/master/dist/radio4000-player.min.js'
+	const { RADIO4000_LOCAL,
+					RADIO4000_DEV,
+					RADIO4000_PROD } = process.env;
+
+	var scriptUrl;
+
+		if (RADIO4000_LOCAL) {
+			scriptUrl = 'http://localhost:5000/dist/radio4000-player.js'
+		} else {
+			scriptUrl = 'https://rawgit.com/internet4000/radio4000-player-vue/master/dist/radio4000-player.min.js'
+		}
 
 	var buffer = fs.readFileSync(process.cwd() + '/templates/embed.html')
 
 	return buffer
 		.toString()
-		.replace('##CHANNEL_SLUG##', slug)
+		.replace(new RegExp('##CHANNEL_SLUG##', 'g'), slug)
 		.replace('##PLAYER_SCRIPT_URL##', scriptUrl)
 }
