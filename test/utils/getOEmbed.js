3147f311595989f8c42e9000048ff12725ab6358
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-06-01 13:41:31 +0200

    add test gor oEmbed

---
+++ test/utils/getOEmbed.js
@@ -0,0 +1,28 @@
+import test from 'ava';
+import getOEmbed from '../../utils/getOEmbed.js';
+
+test('getOEmbed: it returns an object with 12 keys', t => {
+
+	const channel = {
+		slug: '200ok',
+		title: '200ok',
+		body: 'oijoijoij'
+	}
+
+	const embed = getOEmbed('http://test.com', channel)
+	t.is(Object.keys(embed).length, 12)
+})
+
+
+test('getOEmbed: it works with !channel.body', t => {
+
+	const channel = {
+		slug: '200ok',
+		title: '200ok'
+	}
+	const embed = getOEmbed('http://test.com', channel)
+	const embedToString = JSON.stringify(embed)
+	const noUndefined = embedToString.indexOf('undefined')
+	t.is(noUndefined, -1)
+
+})
