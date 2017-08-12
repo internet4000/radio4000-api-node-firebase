9e86c461a5b319dd79a15782349850c8170ff0bc
Oskar Rough <oskar@rough.dk>
2017-08-12 12:24:31 +0200

    Fix test paths

--- test/utils/getIframe.js
+++ test/utils/getIframe.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import getIframe from '../../utils/getIframe.js';
+import getIframe from '../../src/utils/getIframe.js';
 
 test('iframe: it fails without 2 params', t => {
 	t.throws(() => getIframe())
@@ -9,7 +9,7 @@ test('iframe: slug & script placeholder replaced in template', t => {
 	const iframe = getIframe('200ok', 'https://test.com')
 	const replacedSlug = iframe.indexOf('##CHANNEL_SLUG##')
 	const replacedScript = iframe.indexOf('##PLAYER_SCRIPT_URL##')
-	t.is(iframe.length, 185)
+	t.is(iframe.length, 165)
 	t.is(replacedSlug, -1)
 	t.is(replacedScript, -1)
 });
