33518a84b4ee510bb4f26a780a21d5379780b625
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2019-02-27 21:22:44 +0100

    add a function to handle deleted channels

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const functions = require('firebase-functions')
 const app = require('./src/app')
 
+const {database} = functions
+
 exports.api = functions.https.onRequest(app)
+
+// a channel is deleted
+exports.handleChannelDelete = database.ref('/channels/{channelId}')
+	.onDelete((snapshot, context) => {
+		const channel = snapshot.val()
+		const {channelId} = context.params
+
+		return snapshot.ref.parent.parent.child(`/channelPublics/${channel.channelPublic}/followers`)
+			.once('value')
+			.then(followersSnap => {
+				let updates = {}
+				Object.keys(followersSnap.val()).forEach(followerId => {
+					updates[`/channels/${followerId}/favoriteChannels/${channelId}`] = null
+				})
+				return snapshot.ref.parent.parent.update(updates)
+			})
+	})
