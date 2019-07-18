87c1d4af30b0a17c6f407e1d4683bddcf563178a
hugurp <hugurp@protonmail.com>
2019-07-18 12:25:17 +0200

    update image url in oembed

--- src/utils/get-oembed.js
+++ src/utils/get-oembed.js
@@ -1,9 +1,19 @@
 const config = require('../config')
 
-module.exports = function({ slug, title, body }) {
+module.exports = function({ slug, title, body, image }) {
+	var thumbnailUrl;
+
 	if (!body) {
 		body = ''
 	}
+
+	if (image) {
+		thumbnailUrl = `https://res.cloudinary.com/radio4000/image/upload/w_500,h_500,c_thumb,q_60,fl_lossy/${image}`
+	} else {
+		thumbnailUrl =  'https://assets.radio4000.com/icon.png'
+	}
+
+
 	return {
 		version: '1.0',
 		type: 'rich',
@@ -13,7 +23,7 @@ module.exports = function({ slug, title, body }) {
 		author_url: `https://radio4000.com/${slug}/`,
 		title: title,
 		description: body,
-		thumbnail_url: `https://assets.radio4000.com/icon.png`,
+		thumbnail_url: thumbnailUrl,
 		html: `<iframe width="320" height="500" src="${config.apiURL}/embed?slug=${slug}" frameborder="0"></iframe>`,
 		width: 320,
 		height: 500
