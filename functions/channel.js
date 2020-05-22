5c246da147d305d25a8f5443ee8e995f09cc87a2
hugurp <hugurp@pm.me>
2020-05-22 15:38:17 +0200

    use slugify package

--- functions/channel.js
+++ functions/channel.js
@@ -1,19 +1,4 @@
-// Turn a string into a safe "slug". Useful for URLs.
-// Cleans spaces in the beginning or end
-// and dasherizes, which also makes it lowercase
-// and remove special (but normally safe) chars
-// remember not do remove hyphens ----.
-
-const slugify = (string) => {
-	if (!string) {
-		return ''
-	}
-	return string
-		.trim()
-		.dasherize()
-		.replace(/[`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/]/gi, '');
-}
-
+const slugify = require('@sindresorhus/slugify')
 
 const deleteChannelFollowersReferences = async (dbRootRef, channelPublic) => {
 	let channelFollowersRef = dbRootRef.child(`/channelPublics/${channelPublic}/followers`)
