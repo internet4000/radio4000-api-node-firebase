8b559720e7a5ad857edd0c3ebac29cd9167bad2e
hugurp <hugurp@pm.me>
2020-05-25 07:59:52 +0200

    fix slug generation on channel-{create,update}

--- functions/channel.js
+++ functions/channel.js
@@ -24,68 +24,32 @@ const deleteChannelFollowersReferences = async (dbRootRef, channelId, channelPub
 	return dbRootRef.update(updates)
 }
 
+const getUniqueChannelSlug = async (channelData) => {
+	let {title, slug} = channelData
+	slug = slugify(slug || title)
+	let channelsRef = admin.database().ref(`/channels`)
 
-/*
-	 when a channel is deleted
-*/
+	const channelWithSameSlug = await channelsRef
+		.orderByChild('slug')
+		.equalTo(slug)
+		.once(snapshot => snapshot.val())
 
-const handleChannelDelete = async (change, context) => {
-	const channel = change.val()
-	const {id: channelId} = context.params
-	const {auth} = context
-
-	let channelPublic
-	if (channel) {
-		channelPublic = channel.channelPublic
+	if (channelWithSameSlug && channelWithSameSlug.length) {
+		let randomString = Math.random().toString(36).substring(7);
+		return `${slug}-${randomString}`
+	} else {
+		return slug
 	}
-
-	if (!auth) {
-		console.error('Channel delete called without auth')
-		return
-	}
-
-	const {uid} = auth
-
-	if (!channelId || !uid) {
-		console.error('Channel delete called without channelId or auth.uid')
-		return
-	}
-
-	// find current-user at ref: /users/:currentUser
-	let userChannelRef = change.ref
-	let dbRootRef = userChannelRef.parent.parent
-
-	if (channelPublic) {
-		try {
-			await deleteChannelFollowersReferences(dbRootRef, channelId, channelPublic)
-		} catch (error) {
-			console.error('Error deleting channel\'s followers.favorite[channelId] refs')
-		}
-	}
-
-	// find current-channel-public at ref: /channelPublic/:channel.channelPublic
-	let channelPublicRef = dbRootRef.child(`/channelPublics/${channelPublic}`)
-	try {
-		await channelPublicRef.set(null)
-	} catch (error) {
-		console.error('Error deleting /channelPublics/:channel.channelPublic')
-	}
-
-	// find current-user at ref: /users/:currentUser
-	let userRef = dbRootRef.child(`/users/${uid}`)
-	try {
-		await userRef.child(`/channels/${channelId}`).set(null)
-	} catch (error) {
-		console.error('Error removing channel on user')
-	}
-
 }
 
+
+/*
+	 a channel is created
+*/
 const handleChannelCreate = async (snapshot, context) => {
-	const newValue = snapshot.val()
+	const newChannel = snapshot.val()
 	const {id: channelId} = context.params
 	const {auth} = context
-	const {title} = newValue
 
 	if (!auth) {
 		console.error('Channel create called without auth')
@@ -104,9 +68,14 @@ const handleChannelCreate = async (snapshot, context) => {
 	let dbRootRef = userChannelRef.parent.parent
 
 	// validate slug, or generate it
+	let channelSlug
+	try {
+		channelSlug = await getUniqueChannelSlug(newChannel)
+	}
+
 	try {
 		await userChannelRef.update({
-			slug: slugify(title),
+			slug: channelSlug,
 			created: admin.database.ServerValue.TIMESTAMP,
 			updated: admin.database.ServerValue.TIMESTAMP
 		})
@@ -168,26 +137,79 @@ const handleChannelUpdate = async (change, context) => {
 	const newValue = change.after.val()
 	const previousValue = change.before.val()
 
-	if (!newValue.slug) {
+	if (!newValue.slug || newValue.slug !== previousValue.slug) {
+		// validate slug, or generate it
+		let channelSlug
+		try {
+			channelSlug = await getUniqueChannelSlug(newValue)
+		}
+
 		try {
 			await userChannelRef.update({
-				slug: slugify(newValue.title)
+				slug: channelSlug
 			})
 		} catch (error) {
-			console.error('Error setting slug from title')
-		}
-	} else {
-		if (newValue.slug !== previousValue.slug) {
-			try {
-				await userChannelRef.update({
-					slug: slugify(newValue.slug)
-				})
-			} catch (error) {
-				console.error('Error setting slug from slug')
-			}
+			console.error('Error setting slug from channelData')
 		}
 	}
 }
+
+
+/*
+	 when a channel is deleted
+ */
+const handleChannelDelete = async (change, context) => {
+	const channel = change.val()
+	const {id: channelId} = context.params
+	const {auth} = context
+
+	let channelPublic
+	if (channel) {
+		channelPublic = channel.channelPublic
+	}
+
+	if (!auth) {
+		console.error('Channel delete called without auth')
+		return
+	}
+
+	const {uid} = auth
+
+	if (!channelId || !uid) {
+		console.error('Channel delete called without channelId or auth.uid')
+		return
+	}
+
+	// find current-user at ref: /users/:currentUser
+	let userChannelRef = change.ref
+	let dbRootRef = userChannelRef.parent.parent
+
+	if (channelPublic) {
+		try {
+			await deleteChannelFollowersReferences(dbRootRef, channelId, channelPublic)
+		} catch (error) {
+			console.error('Error deleting channel\'s followers.favorite[channelId] refs')
+		}
+	}
+
+	// find current-channel-public at ref: /channelPublic/:channel.channelPublic
+	let channelPublicRef = dbRootRef.child(`/channelPublics/${channelPublic}`)
+	try {
+		await channelPublicRef.set(null)
+	} catch (error) {
+		console.error('Error deleting /channelPublics/:channel.channelPublic')
+	}
+
+	// find current-user at ref: /users/:currentUser
+	let userRef = dbRootRef.child(`/users/${uid}`)
+	try {
+		await userRef.child(`/channels/${channelId}`).set(null)
+	} catch (error) {
+		console.error('Error removing channel on user')
+	}
+
+}
+
 
 module.exports = {
 	handleChannelCreate,
